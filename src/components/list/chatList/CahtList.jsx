a42eccd65fc1412e52b56149c68ceb0ad91e5470
ahmedkhaled90h <ahmedkhaled90h@gmail.com>
2024-05-03 01:53:20 +0300

    finsh the user side

--- src/components/list/chatList/CahtList.jsx
+++ src/components/list/chatList/CahtList.jsx
@@ -1,16 +1,67 @@
+import { useState, useEffect } from 'react';
 import './chatlist.css'
-import { FaSearch } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa";
+import { RxAvatar } from "react-icons/rx";
+import { FaSearch, FaPlus, FaMinus } from "react-icons/fa";
 
 const CahtList = () => {
+
+    
+    const [addBtn, setAddBtn] = useState(true)
     return (
+        
         <div className='chatList'>
-            <div className="">
+            <div className="search">
                 <div className="searchBar">
-                    <FaSearch />
+                    <FaSearch className='icon' />
                     <input type="text" placeholder='Search' />
                 </div>
-                <FaPlus />
+                {addBtn ? (
+                    <FaPlus 
+                        onClick={() => setAddBtn(!addBtn)}
+                        className='add'
+                    />
+                ) : (
+                    <FaMinus 
+                        onClick={() => setAddBtn(!addBtn)}
+                        className='add'
+                    />
+                )}
+            </div>
+            
+            <div className="item">
+                <RxAvatar className='icon' />
+                <div className="texts">
+                    <span>Sire Jow  </span>
+                    <p>Hello for</p>
+                </div>
+            </div>
+            <div className="item">
+                <RxAvatar className='icon' />
+                <div className="texts">
+                    <span>Sire Jow  </span>
+                    <p>Hello for</p>
+                </div>
+            </div>
+            <div className="item">
+                <RxAvatar className='icon' />
+                <div className="texts">
+                    <span>Sire Jow  </span>
+                    <p>Hello for</p>
+                </div>
+            </div>
+            <div className="item">
+                <RxAvatar className='icon' />
+                <div className="texts">
+                    <span>Sire Jow  </span>
+                    <p>Hello for</p>
+                </div>
+            </div>
+            <div className="item">
+                <RxAvatar className='icon' />
+                <div className="texts">
+                    <span>Sire Jow  </span>
+                    <p>Hello for</p>
+                </div>
             </div>
         </div>
     )
