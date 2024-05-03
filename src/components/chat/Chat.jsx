a5069c7b434ecb98948f2bdd965c6d7098d14235
ahmedkhaled90h <ahmedkhaled90h@gmail.com>
2024-05-04 00:09:37 +0300

    start in chat side

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,54 @@
 import './chat.css'
+import { RxAvatar } from "react-icons/rx";
+import { BsEmojiWinkFill } from "react-icons/bs";
+import { 
+    FaPhoneAlt,
+    FaVideo,
+    FaInfoCircle,
+    FaMicrophone,
+    FaCamera,
+    FaImage
+} from "react-icons/fa";
+import EmojiPicker from 'emoji-picker-react'
+import { useState } from 'react';
+
 
 const Chat = () => {
+    const [emojiOpen, setEmojiOpen] = useState(false)
+    const handleEmojiClick = () => {
+        setEmojiOpen(!emojiOpen);
+    };
+
     return (
         <div className='chat'>
-            chat
+            <div className="top">
+                <div className="user">
+                    <RxAvatar className='icon' />
+                    <div className="texts">
+                        <span>Jow sev</span>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <FaPhoneAlt className='icon' />
+                    <FaVideo className='icon' />
+                    <FaInfoCircle className='icon' />
+                </div>
+            </div>
+            <div className="center"></div>
+            <div className="bottom">
+                <div className="icons">
+                    <FaImage className='icon' />
+                    <FaCamera className='icon' />
+                    <FaMicrophone className='icon' />
+                </div>
+                <input type="text" placeholder='Type a message...' />
+                <div className="emoji">
+                    <BsEmojiWinkFill className='icon' onClick={handleEmojiClick} />
+                    {emojiOpen && <EmojiPicker onEmojiClick={console.log} />}
+                </div>
+                <button className='sendButton'>Send</button>
+            </div>
         </div>
     )
 }
